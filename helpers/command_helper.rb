181b9e81636a8e70bec6bb23b2edfa98cdcdb6ac
Koray Koska <koray@koska.at>
2017-04-15 21:00:32 +0200

    Fix answer commands parsing

--- helpers/command_helper.rb
+++ helpers/command_helper.rb
@@ -4,17 +4,26 @@
 module CommandHelper
   def get_commands(json:)
     commands = [Command::Start, Command::ConvertSpeech, Command::ChangeLang]
+    answer_commands = commands
     correct_commands = []
 
     command = json['message']
     command = command['text'] unless command.nil?
     unless command.nil?
       commands.each do |c|
-        if c.command?(command, bot_name: settings.bot_username)
-          correct_commands << c.new(json: json, helpers: self)
-        elsif c.can_answer?(json: json, bot_name: settings.bot_username)
-          correct_commands << c.new(json: json, helpers: self)
-        end
+        next unless c.command?(command, bot_name: settings.bot_username)
+
+        correct_commands << c.new(json: json, helpers: self)
+        # Remove from commands which can potentially be answered
+        # because this is already a command for this command class
+        answer_commands -= [c]
+      end
+    end
+
+    # Commands which can answer this non-command
+    answer_commands.each do |c|
+      if c.can_answer?(json: json, bot_name: settings.bot_username)
+        correct_commands << c.new(json: json, helpers: self)
       end
     end
 
